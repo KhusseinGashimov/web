aa43c733c029c46d3647af22296fe05a8cb3d84a
Gahoo <rmit25@mail.ru>
2024-01-26 12:19:56 +0300

    last

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const infuraApiKey = '18bf6c6f071d4f949e9ecc4a2525d260';
 const web3 = new Web3('https://sepolia.infura.io/v3/18bf6c6f071d4f949e9ecc4a2525d260', { timeout: 60000 });
 
-const contractAddress = '0xc3f423E5d0F409d88bEdF262CF1Dd9A392bDf6cf';
+const contractAddress = '0x6D10F5a04717C0249836d4feF9bdAa9865A83E08';
 
 // Read ABI from JSON file
 const abiFilePath = 'ABI.json';
@@ -14,7 +14,7 @@ const abi = JSON.parse(abiRawData);
 
 const contract = new web3.eth.Contract(abi, contractAddress);
 
-const userAddress = '0x318EED037F6B739524A1Bc663101eAa73f0c9f8C';
+const userAddress = '0x2b1956F7DA27983355eF4FA275C48CD6366F7f64';
 
 
 contract.methods.balanceOf(userAddress).call()
